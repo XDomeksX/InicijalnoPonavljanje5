5e752329a99bd82506a50d4b3f417c1471ed4347
Dome <Dome@DESKTOP-7PFIV6R>
2022-09-16 14:43:20 +0200

    Add project files.

---
+++ InicijalnoPonavljanje5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite link stranice: ");
+            string URL = Console.ReadLine();
+
+            if(URL.StartsWith("www.") && URL.EndsWith(".hr"))
+            {
+                Console.WriteLine("URL je ispravno napisan.");
+            }
+            else
+            {
+                Console.WriteLine("URL nije ispravno napisan.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
